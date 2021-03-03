83ec4c5eeb8947941c6137291479cfc755ab7293
Adam Rosenblum <adamrosenblum@Adams-MBP.attlocal.net>
2021-03-03 09:52:16 -0800

    Finished Player Section and made sure it is working.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[SerializeField]
+	private float _speed = 3.5f;
+	
     void Start()
     {
-        
+	    transform.position = new Vector3(0,-3.0f, 0);
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
-    }
+	{
+		CalculateMovement();
+
+	}
+    
+	void CalculateMovement()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticlInput = Input.GetAxis("Vertical");
+		
+		Vector3 direction = new Vector3(horizontalInput, verticlInput, 0);
+		transform.Translate(direction * _speed * Time.deltaTime);
+		
+		transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
+		
+		if(transform.position.x >=11)
+		{
+			transform.position = new Vector3(-11, transform.position.y, 0);
+		}
+		else if(transform.position.x <= -11)
+		{
+			transform.position = new Vector3(11, transform.position.y, 0);
+		}
+	}
 }
