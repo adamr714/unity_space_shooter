a4e991c4e0daff14a1e94277faef49c05a8eb517
Adam Rosenblum <adamrosenblum@Adams-MBP.attlocal.net>
2021-03-12 16:40:26 -0800

    Game Over text working.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,23 +14,46 @@ public class UIManager : MonoBehaviour
 	[SerializeField]
 	private Image _LivesImg;
 	
+	[SerializeField]
+	private Text _gameOver;
+	
     // Start is called before the first frame update
     void Start()
 	{
-	    _scoreText.text = "Score" + 0;
+		_scoreText.text = "Score: " + 0;
+		_gameOver.gameObject.SetActive(false);
 	    
     }
 
 	public void UpdateScore(int playerScore)
 	{
-		_scoreText.text = "Score" + playerScore.ToString();
+		_scoreText.text = "Score: " + playerScore.ToString();
 	}
 	
 	public void UpdateLives(int currentLives)
 	{
 		
 		_LivesImg.sprite = _liveSprites[currentLives];
+		
+		if(currentLives == 0)
+		{
+			StartCoroutine(BlinkingGameOver());
+		}
 	}
+	
+	IEnumerator BlinkingGameOver()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(.5f);
+			_gameOver.gameObject.SetActive(true);			
+			yield return new WaitForSeconds(.5f);
+			_gameOver.gameObject.SetActive(false);			
+
+		}
+
+	}
+	
 }
 
 
