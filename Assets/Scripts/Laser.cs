14d0048ad08cc25ad5494aef97f1817132083cab
Adam Rosenblum <adamrosenblum@Adams-MBP.attlocal.net>
2021-03-26 09:44:03 -0700

    Extras Completed

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,19 +7,67 @@ public class Laser : MonoBehaviour
 	[SerializeField]
 	private float _speed = 8.0f;
 	
+	private bool _isEnemyLaser = false;
+	
     void Update()
     {
-	    transform.Translate(Vector3.up * _speed * Time.deltaTime);
-	    
-	    if(transform.position.y >= 7)
+	    if(_isEnemyLaser == false)
 	    {
-
-		    if(transform.parent != null)
-	    	{
-	    		Destroy(transform.parent.gameObject);
-	    	}
-	    	
-	    	Destroy(this.gameObject);
+	    	MoveUp();
+	    }
+	    else
+	    {
+	    	MoveDown();
 	    }
     }
+    
+	void MoveUp()
+	{
+		transform.Translate(Vector3.up * _speed * Time.deltaTime);
+	    
+		if(transform.position.y >= 7)
+		{
+
+			if(transform.parent != null)
+			{
+				Destroy(transform.parent.gameObject);
+			}
+	    	
+			Destroy(this.gameObject);
+		}
+	}
+	
+	void MoveDown()
+	{
+		transform.Translate(Vector3.down * _speed * Time.deltaTime);
+	    
+		if(transform.position.y <= -7)
+		{
+
+			if(transform.parent != null)
+			{
+				Destroy(transform.parent.gameObject);
+			}
+	    	
+			Destroy(this.gameObject);
+		}
+	}
+	
+	public void AssignEnemyLaser()
+	{
+		_isEnemyLaser = true;
+	}
+	
+	private void OnTriggerEnter2d(Collider2D other)
+	{
+		if(other.tag == "Player" && _isEnemyLaser == true)
+		{
+			Player player = other.GetComponent<Player>();
+			
+			if(player != null)
+			{
+				player.Damage();
+			}
+		}
+	}
 }
